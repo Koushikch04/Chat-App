ada6d98ffb5a017f90ed043d8ea968f2f45d563c
KOUSHIK CHALLAGUNDLA <chk240404@gmail.com>
2024-12-25 23:01:53 +0530

    completed primary functionalities

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,8 +1,20 @@
 import { auth, provider } from "../firebase-config.js";
 import { signInWithPopup } from "firebase/auth";
-export const Auth = () => {
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
+
+export const Auth = (props) => {
+  const { setIsAuth } = props;
+
   const signInWithGoogle = async () => {
-    await signInWithPopup(auth, provider);
+    try {
+      const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token", result.user.refreshToken);
+      setIsAuth(true);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div className="auth">
---
+++ src/components/Chat.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  serverTimestamp,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "../firebase-config.js";
+import "../styles/Chat.css";
+import { useEffect } from "react";
+
+export const Chat = (props) => {
+  const { room } = props;
+  const [newMesssage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const messagesRef = collection(db, "messages");
+
+  useEffect(() => {
+    const queryMessages = query(
+      messagesRef,
+      where("room", "==", room),
+      orderBy("createdAt")
+    );
+    const unsubScribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessages(messages);
+    });
+    return () => unsubScribe();
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (newMesssage === "") return;
+    await addDoc(messagesRef, {
+      text: newMesssage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room,
+    });
+    setNewMessage("");
+  };
+  return (
+    <div className="chat-app">
+      <div className="header">
+        <h1>Welcome to: {room}</h1>
+      </div>
+      <div className="messages">
+        {messages.map((message) => (
+          <div className="message" key={message.id}>
+            <span className="user">{message.user}</span>
+            {message.text}
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit} className="new-message-form">
+        <input
+          className="new-message-input"
+          placeholder="Type your message here..."
+          onChange={(e) => setNewMessage(e.target.value)}
+          value={newMesssage}
+        />
+        <button type="submit" className="send-button">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
